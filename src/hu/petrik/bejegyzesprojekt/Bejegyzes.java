c391cb5178e0ad3b641f14593268ce458a5ac811
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-09-29 15:11:34 +0200

    bekértem a felhasználótól egy adatot.

--- src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
+++ src/hu/petrik/bejegyzesprojekt/Bejegyzes.java
@@ -34,7 +34,7 @@ public class Bejegyzes {
         return szerkesztve;
     }
 
-    public  void like() {
+    public void like() {
         likeok++;
     }
 
--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -3,12 +3,18 @@ package hu.petrik.bejegyzesprojekt;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Bejegyzes> bejegyzes = new ArrayList<>(2);
 
-       
+        Scanner sc = new Scanner();
+
+        System.out.println("Kérek egy darabszámot");
+        int db = sc.nextInt();
     }
 }
