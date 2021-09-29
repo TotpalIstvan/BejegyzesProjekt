b4e49a9ec53e00cd9c2d03a95986c5e8c0e994b7
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-09-29 15:08:06 +0200

    Lista létrehozása

--- src/hu/petrik/bejegyzesprojekt/Main.java
+++ src/hu/petrik/bejegyzesprojekt/Main.java
@@ -1,8 +1,14 @@
 package hu.petrik.bejegyzesprojekt;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Bejegyzes> bejegyzes = new ArrayList<>(2);
+
+       
     }
 }
